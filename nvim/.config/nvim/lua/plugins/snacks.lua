eafe9a090b9e13c18aab535a7358bdbc0d9232d6
kobebrylant <wojto811@wp.pl>
2026-04-21 16:43:34 +0200

    find in . files by default

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,15 +2,15 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
+      hidden = true,
+      ignored = false, -- set to true if you also want .gitignore'd files (like node_modules)
       sources = {
         explorer = {
           hidden = true,
           ignored = false,
           layout = {
             preset = "sidebar",
-            layout = {
-              width = 30,
-            },
+            layout = { width = 35 },
           },
         },
       },
