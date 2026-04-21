b764c0cfdf7b8d3275de822239a48f8c7ef780fc
kobebrylant <wojto811@wp.pl>
2026-04-21 16:26:08 +0200

    hidden on by defualt

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = false,
+          layout = {
+            preset = "sidebar",
+            layout = {
+              width = 30,
+            },
+          },
+        },
+      },
+    },
+  },
+}
